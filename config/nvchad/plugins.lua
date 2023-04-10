ca21dcd2819343338a43757da5ff42a0a6cb2695
Ali Anari <ali@anari.io>
2023-04-09 22:22:16 -0700

    Add lua TS powered splitjoin plugin to nvim

--- config/nvchad/plugins.lua
+++ config/nvchad/plugins.lua
@@ -349,4 +349,14 @@ return {
 		"saifulapm/chartoggle.nvim",
 		event = "BufEnter",
 	},
+
+	{
+		"Wansmer/treesj",
+		keys = { "<space>m", "<space>j", "<space>s" },
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		config = function()
+			require("treesj").setup({--[[ your config ]]
+			})
+		end,
+	},
 }
