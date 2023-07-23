04bfe0b412a9456879c4451a1bbbc0eadb962131
Ali Anari <ali@anari.io>
2023-07-22 20:34:20 -0700

    Update Mac screen capture location

--- macos2.sh
+++ macos2.sh
@@ -39,8 +39,8 @@ defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 # Screen                                                                      #
 ###############################################################################
 
-# Save screenshots to the desktop
-defaults write com.apple.screencapture location -string "${HOME}/Desktop"
+# Save screenshots to a folder called "Captures"
+defaults write com.apple.screencapture location -string "${HOME}/Captures"
 
 # Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
 defaults write com.apple.screencapture type -string "png"
