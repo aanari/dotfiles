366f8d939a3b7fcacc4f71a93b8a549cc38a344c
Ali Anari <ali@anari.io>
2023-08-07 23:15:12 -0700

    Remove extra indent plugin

--- config/nvchad/plugins.lua
+++ config/nvchad/plugins.lua
@@ -391,14 +391,4 @@ return {
 			},
 		},
 	},
-
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		opts = function(_, opts)
-			return require("indent-rainbowline").make_opts(opts)
-		end,
-		dependencies = {
-			"TheGLander/indent-rainbowline.nvim",
-		},
-	},
 }
