376f8ac0c22894a0756fc109be884e5b3485be00
Ali Anari <ali@anari.io>
2023-08-07 20:09:26 -0700

    Rainbow indent

--- config/nvchad/plugins.lua
+++ config/nvchad/plugins.lua
@@ -148,34 +148,6 @@ return {
 	},
 
 	{
-		"echasnovski/mini.indentscope",
-		version = false, -- wait till new 0.7.0 release to put it back on semver
-		event = { "BufReadPre", "BufNewFile" },
-		opts = {
-			-- symbol = "▏",
-			symbol = "│",
-			options = { try_as_border = true },
-			draw = {
-				delay = 0,
-				animation = function()
-					return 0
-				end,
-			},
-		},
-		init = function()
-			vim.api.nvim_create_autocmd("FileType", {
-				pattern = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
-				callback = function()
-					vim.b.miniindentscope_disable = true
-				end,
-			})
-		end,
-		config = function(_, opts)
-			require("mini.indentscope").setup(opts)
-		end,
-	},
-
-	{
 		"tummetott/reticle.nvim",
 		event = "VeryLazy",
 	},
@@ -417,6 +389,16 @@ return {
 			panel = {
 				enabled = false,
 			},
+		},
+	},
+
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		opts = function(_, opts)
+			return require("indent-rainbowline").make_opts(opts)
+		end,
+		dependencies = {
+			"TheGLander/indent-rainbowline.nvim",
 		},
 	},
 }
