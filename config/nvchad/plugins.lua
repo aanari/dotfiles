8eec328a9db15e238b676043ad7bb491e10b97ba
Ali Anari <ali@anari.io>
2023-08-12 23:09:26 -0700

    Add nvim scrollbar

--- config/nvchad/plugins.lua
+++ config/nvchad/plugins.lua
@@ -509,4 +509,44 @@ return {
 		end,
 		event = "WinEnter",
 	},
+
+	{
+		"Xuyuanp/scrollbar.nvim",
+		event = "WinScrolled",
+		init = function() end,
+		config = function()
+			local scrollbar = require("scrollbar")
+
+			vim.g.scrollbar_right_offset = 0
+			vim.g.scrollbar_excluded_filetypes = { "NvimTree" }
+			vim.g.scrollbar_highlight = {
+				head = "Scrollbar",
+				body = "Scrollbar",
+				tail = "Scrollbar",
+			}
+			vim.g.scrollbar_shape = {
+				head = "▖",
+				body = "▌",
+				tail = "▘",
+			}
+
+			local augroup = vim.api.nvim_create_augroup("Scrollbar", {})
+			vim.api.nvim_create_autocmd({ "CursorMoved", "WinScrolled" }, {
+				group = augroup,
+				callback = function()
+					return scrollbar.show()
+				end,
+			})
+			vim.api.nvim_create_autocmd({
+				"CursorHold",
+				"BufLeave",
+				"FocusLost",
+				"VimResized",
+				"QuitPre",
+			}, {
+				group = augroup,
+				callback = scrollbar.clear,
+			})
+		end,
+	},
 }
