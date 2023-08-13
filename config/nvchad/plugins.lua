bf816f924e80efd69713eeb4a256ae520a6dd098
Ali Anari <ali@anari.io>
2023-08-12 21:45:53 -0700

    Use flash instead of hop to jump around

--- config/nvchad/plugins.lua
+++ config/nvchad/plugins.lua
@@ -269,12 +269,51 @@ return {
 	},
 
 	{
-		"phaazon/hop.nvim",
-		branch = "v2",
-		cmd = "HopWord",
-		config = function()
-			require("hop").setup()
-		end,
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		opts = {},
+		keys = {
+			{
+				"s",
+				mode = { "n", "x", "o" },
+				function()
+					require("flash").jump()
+				end,
+				desc = "Flash",
+			},
+			{
+				"S",
+				mode = { "n", "o", "x" },
+				function()
+					require("flash").treesitter()
+				end,
+				desc = "Flash Treesitter",
+			},
+			{
+				"r",
+				mode = "o",
+				function()
+					require("flash").remote()
+				end,
+				desc = "Remote Flash",
+			},
+			{
+				"R",
+				mode = { "o", "x" },
+				function()
+					require("flash").treesitter_search()
+				end,
+				desc = "Treesitter Search",
+			},
+			{
+				"<c-s>",
+				mode = { "c" },
+				function()
+					require("flash").toggle()
+				end,
+				desc = "Toggle Flash Search",
+			},
+		},
 	},
 
 	{
