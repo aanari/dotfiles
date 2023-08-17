03c8dde4dec39e7623480cedc75900e085d1a3e1
Ali Anari <ali@anari.io>
2023-08-17 00:19:23 -0700

    Add tree hopper plugin

--- config/nvchad/plugins.lua
+++ config/nvchad/plugins.lua
@@ -6,6 +6,14 @@ return {
 	"tpope/vim-sleuth",
 
 	{
+		"mfussenegger/nvim-treehopper",
+		keys = {
+			{ "m", ":<C-U>lua require('tsht').nodes()<CR>", mode = "o" },
+			{ "m", ":lua require('tsht').nodes()<CR>", mode = "v" },
+		},
+	},
+
+	{
 		"hrsh7th/nvim-cmp",
 		event = "InsertEnter",
 		dependencies = {
