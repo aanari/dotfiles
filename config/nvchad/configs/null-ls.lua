70d765c568f777fa58155319297552dd449ea69c
Ali Anari <ali@anari.io>
2023-08-16 17:00:36 -0700

    LSP timeout

--- config/nvchad/configs/null-ls.lua
+++ config/nvchad/configs/null-ls.lua
@@ -69,7 +69,7 @@ null_ls.setup({
 	on_attach = function()
 		vim.api.nvim_create_autocmd("BufWritePre", {
 			callback = function()
-				vim.lsp.buf.format()
+				vim.lsp.buf.format({ timeout_ms = 5000 })
 			end,
 		})
 	end,
