0f4b51f3f555e885feac8108099d8be6e886504c
Ali Anari <ali@anari.io>
2023-08-26 09:58:31 -0700

    Beautiful nvim diagnostic signs

--- config/nvchad/chadrc.lua
+++ config/nvchad/chadrc.lua
@@ -11,4 +11,14 @@ M.ui = {
 
 M.mappings = require("custom.mappings")
 
+local function lspSymbol(name, icon)
+	local hl = "DiagnosticSign" .. name
+	vim.fn.sign_define(hl, { text = icon, numhl = hl, texthl = hl })
+end
+
+lspSymbol("Error", "\u{f146}")
+lspSymbol("Warn", "\u{f14a}")
+lspSymbol("Info", "\u{f14d}")
+lspSymbol("Hint", "\u{f0fd}")
+
 return M
