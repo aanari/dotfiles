a826185bd4a65a971af5a5060fa6dab6b8077d21
Ali Anari <ali@anari.io>
2023-08-30 23:16:07 -0700

    Add treesitter config for graphql to nvim

--- config/nvchad/configs/overrides.lua
+++ config/nvchad/configs/overrides.lua
@@ -12,6 +12,7 @@ M.treesitter = {
 		"dart",
 		"dockerfile",
 		"go",
+		"graphql",
 		"html",
 		"javascript",
 		"json",
@@ -45,10 +46,10 @@ M.treesitter = {
 	incremental_selection = {
 		enable = true,
 		keymaps = {
-			init_selection = '<c-space>',
-			node_incremental = '<c-space>',
-			scope_incremental = '<c-s>',
-			node_decremental = '<M-space>',
+			init_selection = "<c-space>",
+			node_incremental = "<c-space>",
+			scope_incremental = "<c-s>",
+			node_decremental = "<M-space>",
 		},
 	},
 }
